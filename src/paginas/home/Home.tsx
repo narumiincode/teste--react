22130452cbfcbad9d73d691a04916830e40b2ab3
narumiincode <richardkenji3@outlook.com>
2021-10-25 22:05:22 -0300

    introdução ao react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './home.css';
+
+function Home(){
+    return (
+        <>
+        <h1 className= "titulo">Home</h1>
+        <img src="https://imgur.com/gallery/0MZbKdh.jpg" alt="homepage" className= "img"/>
+        </>
+    );
+}
+
+    export default Home;
